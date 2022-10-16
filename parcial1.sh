b743eb9d6bdd4ff9b573efe51d23c61938c37011
Ignacio687 <ignadavidchaves@gmail.com>
2022-10-16 17:20:06 -0300

    indexReady

--- parcial1.sh
+++ parcial1.sh
@@ -11,5 +11,27 @@ unzip repo.zip
 cd Parcial1-SistemasOperativos
 echo $datetime >> index.html
 echo Autor: $(stat -c '%U' /home/mz/so2022/i.chaves/scripts/parcial1/parcial1.sh) >> index.html
-echo Capacidad utilizada de discos rígidos: $( df -h / --output=used)
-echo Cantidad de procesadores del servidor: $()
+echo Capacidad utilizada de discos rígidos: $( df -h / --output=used) >> index.html
+echo Cantidad de procesadores del servidor: $(lscpu | grep 'CPU(s)':) >> index.html
+echo Lista de procesos en ejecución:$'\n' >> index.html
+ps -ef >> index.html
+usersOnLine=`users`
+echo $usersOnLine >> index.html
+alone=1
+for user in $usersOnLine ;
+do
+        if [ $user != $USER ] ;
+        then
+                alone=0
+        fi
+done
+if [ $alone == 0 ] ;
+then
+        echo Somos varios >> index.html
+else
+        echo Solo estoy yo >> index.html
+fi
+echo En mi carpeta personal '(/home/mz/so2022/i.chaves/)' existe el elemento saludo: $'\n'$(ls /home/mz/so2022/i.chaves/) >> index.html
+echo El elemento saludo dice:$'\n'$(cat /home/mz/so2022/i.chaves/saludo) >> index.html
+rm /home/mz/so2022/i.chaves/saludo
+echo En mi carpeta personal '(/home/mz/so2022/i.chaves/)' ya no existe el elemento saludo: $'\n'$(ls /home/mz/so2022/i.chaves/) >> index.html
